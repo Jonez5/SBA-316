9b67cac239498d0b82c4385ba762b1373ef44b54
Jonez5 <elijahadell8@gmail.com>
2024-10-08 11:06:45 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -26,3 +26,32 @@ listItems.forEach((item) => {
 listItems.forEach((item) => {
     item.addEventListener("mouseout",colorReset);
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const pollForm = document.getElementById("question");
+    const yesCount = document.getElementById("yes-count");
+    const noCount = document.getElementById("no-count");
+    let yesVotes = 0;
+    let noVotes = 0;
+  
+    pollForm.addEventListener("submit", function (e) {
+  
+        e.preventDefault();
+        const formData = new FormData(pollForm);
+        const userVote = formData.get("vote");
+  
+        if (userVote === "yes") {
+            yesVotes++;
+        } else if (userVote === "no") {
+            noVotes++;
+        }
+        updateResults();
+    });
+  
+    function updateResults() {
+        yesCount.textContent = yesVotes;
+        noCount.textContent = noVotes;
+    }
+    
+  })

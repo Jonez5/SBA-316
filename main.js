7da10eef7170de2e9a1029d9c2983d5392e15d6d
Jonez5 <elijahadell8@gmail.com>
2024-10-09 10:37:27 -0400

    adjusted

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Created element using DOM
- const paragraph = document.createElement("p")
- const desheader = document.getElementById("sweets")
- desheader.style.color = "red";
+const paragraph = document.createElement("p")
+const desheader = document.getElementById("sweets")
+desheader.style.color = "red";
 const foodClass = document.querySelectorAll(".food")
 const newLi = document.createElement("li")
 newLi.textContent = "Banana pudding";
@@ -12,46 +12,46 @@ uList.appendChild(newLi)
 
 
 function colorChange(event) {
-    event.target.style.color = "purple"
+   event.target.style.color = "purple"
 }
 
 function colorReset(event) {
-    event.target.style.color = "aqua"
+   event.target.style.color = "aqua"
 }
 
 listItems.forEach((item) => {
-    item.addEventListener("mouseover",colorChange);
+   item.addEventListener("mouseover",colorChange);
 });
 
 listItems.forEach((item) => {
-    item.addEventListener("mouseout",colorReset);
+   item.addEventListener("mouseout",colorReset);
 });
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    const pollForm = document.getElementById("question");
-    const yesCount = document.getElementById("yes-count");
-    const noCount = document.getElementById("no-count");
-    let yesVotes = 0;
-    let noVotes = 0;
-  
-    pollForm.addEventListener("submit", function (e) {
-  
-        e.preventDefault();
-        const formData = new FormData(pollForm);
-        const userVote = formData.get("vote");
-  
-        if (userVote === "yes") {
-            yesVotes++;
-        } else if (userVote === "no") {
-            noVotes++;
-        }
-        updateResults();
-    });
-  
-    function updateResults() {
-        yesCount.textContent = yesVotes;
-        noCount.textContent = noVotes;
-    }
-    
-  })
+   const pollForm = document.getElementById("question");
+   const yesCount = document.getElementById("yes-count");
+   const noCount = document.getElementById("no-count");
+   let yesVotes = 0;
+   let noVotes = 0;
+ 
+   pollForm.addEventListener("submit", function (e) {
+ 
+       e.preventDefault();
+       const formData = new FormData(pollForm);
+       const userVote = formData.get("vote");
+ 
+       if (userVote === "yes") {
+           yesVotes++;
+       } else if (userVote === "no") {
+           noVotes++;
+       }
+       updateResults();
+   });
+ 
+   function updateResults() {
+       yesCount.textContent = yesVotes;
+       noCount.textContent = noVotes;
+   }
+   
+ })
